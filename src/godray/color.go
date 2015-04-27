0d8c078090056c13ac59e16ee2c1afb9d0fed7be
paul <paul@inkling.com>
2015-04-26 17:15:15 -0700

    missing file

---
+++ src/godray/color.go
@@ -0,0 +1,14 @@
+package godray
+
+import (
+	"image/color"
+)
+
+// colors
+var (
+	Red   = &Color{color.RGBA{255, 0, 0, 255}}
+	Green = &Color{color.RGBA{0, 255, 0, 255}}
+	Blue  = &Color{color.RGBA{0, 0, 255, 255}}
+	White = &Color{color.RGBA{255, 255, 255, 255}}
+	Black = &Color{color.RGBA{0, 0, 0, 255}}
+)
